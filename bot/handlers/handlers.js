f5b08c95f84fb2a1f030f3da3354fc0e8126798e
Yonatan-Ethiopia <83534269+Yonatan-Ethiopia@users.noreply.github.com>
2025-06-12 08:19:09 +0000

    Added function for adding products

--- bot/handlers/handlers.js
+++ bot/handlers/handlers.js
@@ -1,4 +1,6 @@
-
+let isAdding = false;
+let step = 'none';
+let newProduct = {}
 const start = (bot, msg)=>{
   bot.sendMessage( msg.chat.id, "Hello! Use the following commands to interact with me.\n /start - Start the bot\n /help - Get help\n /list - List all products\n /available - List available products")}
 const help = (bot, msg)=>{
@@ -11,7 +13,24 @@ const available = (bot, msg, data)=>{
   const Ndata = data.find({ status: 'available' });
   bot.sendMessage( msg.chat.id, Ndata)
 }; 
+const initiate_Add = (bot, msg)=>{
+  isAdding = true;
+  newProduct[msg.chat.id] = {}
+  step = 'name';
+  bot.sendMessage( msg.chat.id, "What is the name of the product");
+}
 const add = (bot, msg)=>{
-  
+  if(isAdding){
+    if(step == 'name')
+      const name = msg.text;
+      newProduct.name = name;
+      step = 'price';
+      bot.sendMessage( msg.chat.id,"What is the price of the product ?")
+    else if(step == 'price'){
+      newProduct.price = msg.text;
+      step = 'category';
+      bot.sendMessage( msg.chat.id,"What is the category of the product ?")
+    }
+  }
 }
 module.exports = { start, help, list, available}
