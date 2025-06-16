f1786e5b99bfa6faf0027c1778f6b814a1ebf4c8
Yonatan-Ethiopia <83534269+Yonatan-Ethiopia@users.noreply.github.com>
2025-06-16 11:44:32 +0000

    modified the schema

--- models/productsModel.js
+++ models/productsModel.js
@@ -3,7 +3,7 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true},
   price: { type: String, required: true},
   category: { type: String, required: true},
-  status: { type: String, enum: ['available', 'unavailable'], default: 'available'},
+  status: { type: String, enum: ['Available', 'Unavailable'], default: 'Available'},
   stock: { type: Number, default: 1},
   image: { type : String, required : true},
   createdAt: { type: Date, default: Date.now, timestamps: true }
