990d3cdbfb141bbea0cc7cb6f36791dc6ab83424
Yonatan-Ethiopia <83534269+Yonatan-Ethiopia@users.noreply.github.com>
2025-06-23 09:04:23 +0000

    added authHandler for authenticating admins, owner, and user

--- bot/handlers/authHandler.js
+++ bot/handlers/authHandler.js
@@ -1,25 +1,16 @@
-const owners = require('../../models/ownerModel');
-const admins = require('../../models/adminsModel');
-const channels = require('../../models/channelsModel');
-const isOwner = async (bot, msg, id)=>{
-  const owner = await owners.findOne({id: id});
-  if(!owner){
-    bot.sendMessage(msg.chat.id, "Permission denied");
-    return false;
-  }else if(owner){
-    return true;
+const owners = require("../../models/ownerModel");
+const admins = require("../../models/adminsModel");
+let role = "";
+const authId = async (msg) => {
+  const isOwner = await owners.findOne({ id: msg.from.id });
+  const isAdmin = await admins.findOne({ id: msg.from.id });
+  if (!isOwner && !isAdmin) {
+    role = "user";
+  } else if (isOwner) {
+    role = "owner";
+  } else if (isAdmin) {
+    role = "admin";
   }
-  
-}
-const isAdmin = async (bot, msg, id)=>{
-  const admin = await admins.findOne({id: id});
-  if(!admin){
-    bot.sendMessage(msg.chat.id, "Permission denied");
-    return false;
-  }else if(admin){
-    const page = await admins.findById(admin._id).populate('page');
-    return true, page;
-    
-  }
-}
-module.exports = { isOwner, isAdmin };
+  return role;
+};
+module.exports = { authId };
